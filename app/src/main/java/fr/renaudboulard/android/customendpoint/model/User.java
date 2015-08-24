6b81da8317d7f71fd583d9544dc0309a627d7ecf
renaudboulard <boulard.renaud@gmail.com>
2015-08-24 21:06:10 +0200

    Rest interface + model

---
+++ app/src/main/java/fr/renaudboulard/android/customendpoint/model/User.java
@@ -0,0 +1,12 @@
+package fr.renaudboulard.android.customendpoint.model;
+
+import java.io.Serializable;
+
+/**
+ * Created by renaud on 24/08/15.
+ */
+public class User implements Serializable {
+
+    public String firstname;
+    public String lastname;
+}
